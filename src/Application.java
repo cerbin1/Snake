228fe49e28245605dae983511df32452f728069b
Bartek <bartek9393@poczta.onet.pl>
2016-11-18 20:55:24 +0100

    Created simple frame

--- src/Application.java
+++ src/Application.java
@@ -1,2 +1,20 @@
-public class Application {
+import javax.swing.*;
+import java.awt.*;
+
+public class Application extends JComponent{
+    public static void main(String[] args) {
+        new Application().run();
+    }
+
+    private void run() {
+        JFrame frame = new JFrame("Snake");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public void paint(Graphics g) {
+        g.drawRect(10, 10, 200, 200);
+    }
 }
