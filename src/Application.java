206f69464cf498f530092acce405bad76226ba59
Bartek <bartek9393@poczta.onet.pl>
2016-11-19 18:48:47 +0100

    fix

--- src/Application.java
+++ src/Application.java
@@ -1,20 +1,52 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Application extends JComponent{
+public class Application extends JComponent implements KeyListener{
+
+    JFrame frame;
     public static void main(String[] args) {
         new Application().run();
     }
 
     private void run() {
-        JFrame frame = new JFrame("Snake");
+        frame = new JFrame("Snake");
+
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
         frame.setLocationRelativeTo(null);
+//        frame.getContentPane().add(new MyCanvas(0));
         frame.setVisible(true);
+        Graphics g = frame.getGraphics();
+        g.setColor(Color.black);
+        g.drawRect(0, 0, 100, 100);
+        g.fillRect(0, 0, 100, 100);
+
+
+        frame.addKeyListener(this);
     }
 
-    public void paint(Graphics g) {
-        g.drawRect(10, 10, 200, 200);
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if(key == 32) {
+            System.out.println("dobrze");
+            frame.getContentPane().add(new MyCanvas(10));
+        }
+        else {
+            System.out.println("dziala");
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
