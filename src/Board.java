859b7ead7cf6e05b8263034b985566c815cd920a
Bartek <bartek9393@poczta.onet.pl>
2017-02-10 21:51:21 +0100

    Added class Board

---
+++ src/Board.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Board extends JComponent{
+    public void paint(Graphics g) {
+        g.drawRect(0, 0, 400, 400);
+        g.setColor(Color.gray);
+        g.fillRect(0, 0, 400, 400);
+        g.drawRect(0, 0, 10, 10);
+        g.setColor(Color.green);
+        g.fillRect(0, 0, 10, 10);
+    }
+}
