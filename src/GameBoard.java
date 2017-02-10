ce1c9b78c96e233590a866bbd5349eeddbc0c817
Bartek <bartek9393@poczta.onet.pl>
2017-02-10 19:23:05 +0100

    Added createJPanel()

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,15 +1,25 @@
 import javax.swing.*;
+import java.awt.*;
 
 class GameBoard {
 
     void displayGameBoard() {
         JFrame frame = createJFrame();
+        JPanel jPanel = createJPanel();
+        frame.add(jPanel);
     }
 
     private JFrame createJFrame() {
         JFrame frame = new JFrame();
-        frame.setSize(200, 200);
+        frame.setSize(300, 300);
         frame.setVisible(true);
         return frame;
+    }
+
+    private JPanel createJPanel() {
+        JPanel jPanel = new JPanel();
+        jPanel.setSize(30, 40);
+        jPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        return jPanel;
     }
 }
