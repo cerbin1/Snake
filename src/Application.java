a58c3ee5f5bd05feb647001233e530687c8f5dbd
Bartek <bartek9393@poczta.onet.pl>
2017-02-11 19:59:25 +0100

    Added rendering map

--- src/Application.java
+++ src/Application.java
@@ -1,12 +1,25 @@
 import javax.swing.*;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
-public class Application {
+public class Application implements ActionListener{
+    private JPanel jPanel;
+    private Head head;
+
+    private Application() {
+        Timer timer = new Timer(10, this);
+        head = new Head(10, 10);
+        jPanel = new GameJPanel(head).createJPanel();
+        displayFrame();
+        timer.start();
+    }
 
     private void displayFrame() {
         JFrame jFrame = createJFrame();
-        jFrame.add(new GameJPanel().createJPanel());
+        jFrame.add(jPanel);
     }
 
     private JFrame createJFrame() {
@@ -20,6 +33,16 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new Application().displayFrame());
+        SwingUtilities.invokeLater(() -> new Application());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("dupa");
+        jPanel.removeAll();
+        head.setY(head.getY() + 5);
+        jPanel.add(new Board(head.getX(), head.getY()));
+        jPanel.repaint();
+        jPanel.revalidate();
     }
 }
