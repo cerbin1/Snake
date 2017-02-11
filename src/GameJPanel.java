4003ad6e40190c9bcf2fe53d713ee975b79462be
Bartek <bartek9393@poczta.onet.pl>
2017-02-11 20:08:20 +0100

    Added move on key click

--- src/GameJPanel.java
+++ src/GameJPanel.java
@@ -4,14 +4,19 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 class GameJPanel {
-    private int x = 10, y = 10;
+    private Head head;
+    private JPanel jPanel;
 
-    javax.swing.JPanel createJPanel() {
-        javax.swing.JPanel jPanel = new javax.swing.JPanel();
+    GameJPanel(Head head) {
+        this.head = head;
+    }
+
+    JPanel createJPanel() {
+        jPanel = new JPanel();
         jPanel.setLayout(new BorderLayout());
         jPanel.setSize(new Dimension(400, 400));
         jPanel.setBorder(BorderFactory.createLineBorder(Color.black, 10));
-        jPanel.add(new Board(x, y), BorderLayout.CENTER);
+        jPanel.add(new Board(head.getX(), head.getY()), BorderLayout.CENTER);
         jPanel.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
@@ -22,32 +27,36 @@ class GameJPanel {
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == 37) {
                     System.out.println("lewo");
+                    head.setX(head.getX() - 5);
                     jPanel.removeAll();
-                    jPanel.add(new Board(x -= 5, y), BorderLayout.CENTER);
+                    jPanel.add(new Board(head.getX(), head.getY()), BorderLayout.CENTER);
                     jPanel.revalidate();
                     jPanel.repaint();
                 }
 
                 if (e.getKeyCode() == 39) {
                     System.out.println("prawo");
+                    head.setX(head.getX() + 5);
                     jPanel.removeAll();
-                    jPanel.add(new Board(x += 5, y), BorderLayout.CENTER);
+                    jPanel.add(new Board(head.getX(), head.getY()), BorderLayout.CENTER);
                     jPanel.revalidate();
                     jPanel.repaint();
                 }
 
                 if (e.getKeyCode() == 40) {
                     System.out.println("dol");
+                    head.setY(head.getY() + 5);
                     jPanel.removeAll();
-                    jPanel.add(new Board(x, y += 5), BorderLayout.CENTER);
+                    jPanel.add(new Board(head.getX(), head.getY()), BorderLayout.CENTER);
                     jPanel.revalidate();
                     jPanel.repaint();
                 }
 
                 if (e.getKeyCode() == 38) {
                     System.out.println("gora");
+                    head.setY(head.getY() - 5);
                     jPanel.removeAll();
-                    jPanel.add(new Board(x, y -= 5), BorderLayout.CENTER);
+                    jPanel.add(new Board(head.getX(), head.getY()), BorderLayout.CENTER);
                     jPanel.revalidate();
                     jPanel.repaint();
                 }
