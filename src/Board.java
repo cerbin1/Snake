8ef452f09e04e3cf23e85a34024372ebcfdee159
Bartek <bartek9393@poczta.onet.pl>
2017-02-11 16:40:37 +0100

    Added moving snake on key clicks

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
 
-class BoardDrawer extends JComponent {
+class Board extends JComponent {
+    private int x, y;
+
+    Board(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     public void paint(Graphics g) {
         g.drawRect(0, 0, 400, 400);
         g.setColor(Color.gray);
         g.fillRect(0, 0, 400, 400);
-        g.drawRect(0, 0, 10, 10);
+        g.drawRect(x, y, 10, 10);
         g.setColor(Color.green);
-        g.fillRect(0, 0, 10, 10);
+        g.fillRect(x, y, 10, 10);
     }
 }
--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,14 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 class GameFrame {
+    private int x = 10, y = 10;
 
     void displayGameFrame() {
         JFrame frame = createJFrame();
-        JPanel jPanel = createJPanel();
-        frame.add(jPanel);
+        frame.setContentPane(createJPanel());
     }
 
     private JFrame createJFrame() {
@@ -26,7 +28,54 @@ class GameFrame {
         jPanel.setLayout(new BorderLayout());
         jPanel.setSize(new Dimension(400, 400));
         jPanel.setBorder(BorderFactory.createLineBorder(Color.black, 10));
-        jPanel.add(new BoardDrawer(), BorderLayout.CENTER);
+        jPanel.add(new Board(x, y), BorderLayout.CENTER);
+        jPanel.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == 37) {
+                    System.out.println("lewo");
+                    jPanel.removeAll();
+                    jPanel.add(new Board(x -= 5, y), BorderLayout.CENTER);
+                    jPanel.revalidate();
+                    jPanel.repaint();
+                }
+
+                if (e.getKeyCode() == 39) {
+                    System.out.println("prawo");
+                    jPanel.removeAll();
+                    jPanel.add(new Board(x += 5, y), BorderLayout.CENTER);
+                    jPanel.revalidate();
+                    jPanel.repaint();
+                }
+
+                if (e.getKeyCode() == 40) {
+                    System.out.println("dol");
+                    jPanel.removeAll();
+                    jPanel.add(new Board(x, y += 5), BorderLayout.CENTER);
+                    jPanel.revalidate();
+                    jPanel.repaint();
+                }
+
+                if (e.getKeyCode() == 38) {
+                    System.out.println("gora");
+                    jPanel.removeAll();
+                    jPanel.add(new Board(x, y -= 5), BorderLayout.CENTER);
+                    jPanel.revalidate();
+                    jPanel.repaint();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                System.out.println(e.getKeyChar());
+            }
+        });
+        jPanel.setFocusable(true);
         return jPanel;
     }
 }
