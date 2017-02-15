ee78d4ef89875a5ad3ad66f666e9c34dff87ad86
Bartek <bartek9393@poczta.onet.pl>
2017-02-15 20:29:27 +0100

    Moved keyListeners and createJPanel()

--- src/snake/Application.java
+++ src/snake/Application.java
@@ -1,27 +1,31 @@
 package snake;
 
 import javax.swing.*;
-
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
-public class Application implements ActionListener {
+public class Application implements ActionListener, KeyListener {
     private JPanel jPanel;
     private Head head;
 
-    private Application() {
-        Timer timer = new Timer(10, this);
-        head = new Head(10, 10);
-        jPanel = new GameJPanel(head).createJPanel();
-        displayFrame();
-        timer.start();
-    }
+    int test = 0;
 
-    private void displayFrame() {
+    ArrayList<Point> snakeParts = new ArrayList<>();
+
+    Application() {
+        Timer timer = new Timer(10, this);
+        head = new Head(0, 0);
+        jPanel = createJPanel();
+        snakeParts.add(new Point(head.getX(), head.getY()));
         JFrame jFrame = createJFrame();
         jFrame.add(jPanel);
+        timer.start();
     }
 
     private JFrame createJFrame() {
@@ -34,16 +38,44 @@ public class Application implements ActionListener {
         return frame;
     }
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new Application());
+    private JPanel createJPanel() {
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new BorderLayout());
+        jPanel.setSize(new Dimension(400, 400));
+        jPanel.setBorder(BorderFactory.createLineBorder(Color.black, 10));
+        jPanel.add(new Board(head.getX(), head.getY(), this), BorderLayout.CENTER);
+        jPanel.setFocusable(true);
+        return jPanel;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("dupa");
         jPanel.removeAll();
-        jPanel.add(new Board(head.getX(), head.getY()));
+        jPanel.add(new Board(head.getX(), head.getY(), this));
+        snakeParts.add(new Point(head.getX(), head.getY() + test));
+        snakeParts.remove(0);
+        test++;
         jPanel.repaint();
         jPanel.revalidate();
+        System.out.println(snakeParts);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Application());
     }
 }
