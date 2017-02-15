194e8cf5b00fb86baf7f659beeba214ff65cbe78
Bartek <bartek9393@poczta.onet.pl>
2017-02-15 20:07:41 +0100

    Moved KeyListener to Application

--- src/snake/GameJPanel.java
+++ src/snake/GameJPanel.java
@@ -1,70 +1,25 @@
 package snake;
 
-import snake.Board;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 class GameJPanel {
     private Head head;
-    private JPanel jPanel;
 
-    GameJPanel(Head head) {
+    private Application application;
+
+    GameJPanel(Head head, Application application) {
         this.head = head;
+        this.application = application;
     }
 
     JPanel createJPanel() {
-        jPanel = new JPanel();
+        JPanel jPanel = new JPanel();
         jPanel.setLayout(new BorderLayout());
         jPanel.setSize(new Dimension(400, 400));
         jPanel.setBorder(BorderFactory.createLineBorder(Color.black, 10));
-        jPanel.add(new Board(head.getX(), head.getY()), BorderLayout.CENTER);
-        jPanel.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-
-            }
-
-            @Override
-            public void keyPressed(KeyEvent e) {
-                if (e.getKeyCode() == 37) {
-                    System.out.println("lewo");
-                    move(-5, 0);
-                }
-
-                if (e.getKeyCode() == 39) {
-                    System.out.println("prawo");
-                    move(5, 0);
-                }
-
-                if (e.getKeyCode() == 40) {
-                    System.out.println("dol");
-                    move(0, 5);
-                }
-
-                if (e.getKeyCode() == 38) {
-                    System.out.println("gora");
-                    move(0, -5);
-                }
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-                System.out.println(e.getKeyChar());
-            }
-        });
+        jPanel.add(new Board(head.getX(), head.getY(), application), BorderLayout.CENTER);
         jPanel.setFocusable(true);
         return jPanel;
-    }
-
-    private void move(int x, int y) {
-        head.setX(head.getX() + x);
-        head.setY(head.getY() + y);
-        jPanel.removeAll();
-        jPanel.add(new Board(head.getX(), head.getY()), BorderLayout.CENTER);
-        jPanel.revalidate();
-        jPanel.repaint();
     }
 }
