36299cfcac50e4dcccb95dc2bd1ad3b059619920
Bartek <bartek9393@poczta.onet.pl>
2017-02-19 23:35:31 +0100

    Refactored enum Direction

--- src/snake/Direction.java
+++ src/snake/Direction.java
@@ -3,53 +3,31 @@ package snake;
 import java.awt.*;
 
 enum Direction {
-    DOWN {
-        @Override
-        public void move(Application application) {
-            if (horizontal(application, 0, 1)) {
-                application.stopGame();
-            } else {
-                addNewSnakePart(application, 0, 1);
-            }
-        }
-    }, UP {
-        @Override
-        public void move(Application application) {
-            if (horizontal(application, 0, -1)) {
-                application.stopGame();
-            } else {
-                addNewSnakePart(application, 0, -1);
-            }
-        }
-    }, LEFT {
-        @Override
-        public void move(Application application) {
-            if (vertical(application, -1, 0)) {
-                application.stopGame();
-            } else {
-                addNewSnakePart(application, -1, 0);
-            }
-        }
-    }, RIGHT {
-        public void move(Application application) {
-            if (vertical(application, 1, 0)) {
-                application.stopGame();
-            } else {
-                addNewSnakePart(application, 1, 0);
-            }
-        }
-    };
+    DOWN(0, 1), UP(0, -1), LEFT(-1, 0), RIGHT(1, 0);
 
-    abstract void move(Application application);
+    private int x, y;
 
-    private static boolean horizontal(Application application, int x, int y) {
-        int nextPartOfSnake = application.getHead().y + y;
-        return nextPartOfSnake >= 40 || nextPartOfSnake < 0 || application.isPartOfSnakeOnPoint(x, y);
+    Direction(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    private static boolean vertical(Application application, int x, int y) {
-        int nextPartOfSnake = application.getHead().x + x;
-        return nextPartOfSnake >= 40 || nextPartOfSnake < 0 || application.isPartOfSnakeOnPoint(x, y);
+    public void move(Application app) {
+        if (horizontalOrVertical(app, x, y)) {
+            app.stopGame();
+        } else {
+            addNewSnakePart(app, x, y);
+        }
+    }
+
+    boolean horizontalOrVertical(Application app, int x, int y) {
+        if (x == 0) {
+            int nextPartOfSnake = app.getHead().y + y;
+            return nextPartOfSnake >= 40 || nextPartOfSnake < 0 || app.isPartOfSnakeOnPoint(x, y);
+        } else {
+            int nextPartOfSnake = app.getHead().x + x;
+            return nextPartOfSnake >= 40 || nextPartOfSnake < 0 || app.isPartOfSnakeOnPoint(x, y);
+        }
     }
 
     private static void addNewSnakePart(Application application, int x, int y) {
