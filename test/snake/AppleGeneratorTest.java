1f16f416a0dec545a36221050867df633675c8ac
Bartek <bartek9393@poczta.onet.pl>
2017-02-23 22:11:12 +0100

    Added AppleGeneratorTest

---
+++ test/snake/AppleGeneratorTest.java
@@ -0,0 +1,20 @@
+package snake;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppleGeneratorTest {
+    @Test
+    public void shouldRelocateApple() {
+        // given
+        AppleGenerator appleGenerator = new AppleGenerator(400, 400);
+
+        // when
+
+
+        // then
+
+    }
+
+}
