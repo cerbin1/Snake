3c7bd09b3a74f61d77f4de96e46dddc4e23c709b
Daniel <wilkowski.kontakt@gmail.com>
2017-02-25 13:09:17 +0100

    Refactored Direction

--- src/snake/Direction.java
+++ src/snake/Direction.java
@@ -2,13 +2,18 @@ package snake;
 
 import java.awt.*;
 
-enum Direction {
-    DOWN(0, 1), UP(0, -1), LEFT(-1, 0), RIGHT(1, 0);
-    private final int x, y;
+import static java.awt.event.KeyEvent.*;
+import static java.util.Arrays.stream;
 
-    Direction(int x, int y) {
+enum Direction {
+    DOWN(0, 1, VK_DOWN), UP(0, -1, VK_UP), LEFT(-1, 0, VK_LEFT), RIGHT(1, 0, VK_RIGHT);
+
+    private final int x, y, keyCode;
+
+    Direction(int x, int y, int keyCode) {
         this.x = x;
         this.y = y;
+        this.keyCode = keyCode;
     }
 
     public void move(Snake snake, Application application) {
@@ -22,5 +27,16 @@ enum Direction {
     private void addNewSnakePart(Snake snake) {
         Point headAfterMove = new Point(snake.getHead().x + x, snake.getHead().y + y);
         snake.setHead(headAfterMove);
+    }
+
+    public boolean isOpposite(Direction direction) {
+        return direction.x == -x && direction.y == -y;
+    }
+
+    public static Direction fromKeyCode(int keyCode) {
+        return stream(values())
+                .filter(direction -> direction.keyCode == keyCode)
+                .findAny()
+                .get();
     }
 }
