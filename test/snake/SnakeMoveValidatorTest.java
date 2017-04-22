78bfe7f16c0c29b7987ef9b5f8aaa351b936db69
Bartek <bartek9393@poczta.onet.pl>
2017-04-22 22:38:47 +0100

    Extracted method snakeMoveValidator()

--- test/snake/SnakeMoveValidatorTest.java
+++ test/snake/SnakeMoveValidatorTest.java
@@ -6,14 +6,11 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class SnakeMoveValidatorTest {
-    private final int lengthOfSnake = 10;
-    private final Size size = new Size(40, 40);
-
     @Test
     public void shouldSnakeWMoveWhenAfterMoveIsInBoard() {
         // given
         Snake snake = snake();
-        SnakeMoveValidator snakeMoveValidator = new SnakeMoveValidator(snake, size);
+        SnakeMoveValidator snakeMoveValidator = snakeMoveValidator(snake);
 
         // when
         boolean valid = snakeMoveValidator.isValid(4, 7);
@@ -22,15 +19,11 @@ public class SnakeMoveValidatorTest {
         assertTrue(valid);
     }
 
-    private static Snake snake() {
-        return new Snake(10, new Size(40, 40));
-    }
-
     @Test
     public void shouldSnakeMoveWhenNextMoveExceedBottomBorder() {
         // given
         Snake snake = snake();
-        SnakeMoveValidator snakeMoveValidator = new SnakeMoveValidator(snake, size);
+        SnakeMoveValidator snakeMoveValidator = snakeMoveValidator(snake);
 
         // when
         boolean valid = snakeMoveValidator.isValid(50, 0);
@@ -43,7 +36,7 @@ public class SnakeMoveValidatorTest {
     public void shouldSnakeMoveWhenNextMoveExceedTopBorder() {
         // given
         Snake snake = snake();
-        SnakeMoveValidator snakeMoveValidator = new SnakeMoveValidator(snake, size);
+        SnakeMoveValidator snakeMoveValidator = snakeMoveValidator(snake);
 
         // when
         boolean valid = snakeMoveValidator.isValid(-12, 0);
@@ -56,7 +49,7 @@ public class SnakeMoveValidatorTest {
     public void shouldSnakeMoveWhenNextMoveExceedRightBorder() {
         // given
         Snake snake = snake();
-        SnakeMoveValidator snakeMoveValidator = new SnakeMoveValidator(snake, size);
+        SnakeMoveValidator snakeMoveValidator = snakeMoveValidator(snake);
 
         // when
         boolean valid = snakeMoveValidator.isValid(0, 60);
@@ -69,7 +62,7 @@ public class SnakeMoveValidatorTest {
     public void shouldSnakeMoveWhenNextMoveExceedLeftBorder() {
         // given
         Snake snake = snake();
-        SnakeMoveValidator snakeMoveValidator = new SnakeMoveValidator(snake, size);
+        SnakeMoveValidator snakeMoveValidator = snakeMoveValidator(snake);
 
         // when
         boolean valid = snakeMoveValidator.isValid(0, -5);
@@ -83,12 +76,20 @@ public class SnakeMoveValidatorTest {
         // given
         Snake snake = snake();
         snake.addHead();
-        SnakeMoveValidator snakeMoveValidator = new SnakeMoveValidator(snake, size);
+        SnakeMoveValidator snakeMoveValidator = snakeMoveValidator(snake);
 
         // when
         boolean valid = snakeMoveValidator.isValid(0, 0);
 
         // then
         assertFalse(valid);
+    }
+
+    private static Snake snake() {
+        return new Snake(10, new Size(40, 40));
+    }
+
+    private static SnakeMoveValidator snakeMoveValidator(Snake snake) {
+        return new SnakeMoveValidator(snake, new Size(40, 40));
     }
 }
